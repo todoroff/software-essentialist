df04db21224c52b472360e1bf0477084fbfefb91
todoroff <48935314+todoroff@users.noreply.github.com>
2023-03-19 23:43:34 +0200

    🔄 test invalid inputs that are closer to or farther from the valid range

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -6,11 +6,12 @@ describe("fizzbuzz", () => {
 
   it("takes only numbers from 1 to 100", () => {
     expect(typeof fizzBuzz(1)).toBe("string");
-    expect(() => fizzBuzz(-1)).toThrowError();
-    expect(() => fizzBuzz(-25)).toThrowError();
+    expect(() => fizzBuzz(0.99)).toThrowError();
+    expect(() => fizzBuzz(-5)).toThrowError();
     expect(typeof fizzBuzz(25)).toBe("string");
     expect(typeof fizzBuzz(100)).toBe("string");
-    expect(() => fizzBuzz(101)).toThrowError();
+    expect(() => fizzBuzz(100.01)).toThrowError();
+    expect(() => fizzBuzz(105)).toThrowError();
   });
 
   it("returns the input number as a string", () => {
