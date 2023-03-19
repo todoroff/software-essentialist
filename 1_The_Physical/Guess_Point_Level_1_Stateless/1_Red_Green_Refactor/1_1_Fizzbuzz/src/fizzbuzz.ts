63829efe1f9a9462c6d9ce16cbf544adac6a2fc0
todoroff <48935314+todoroff@users.noreply.github.com>
2023-03-19 21:53:11 +0200

    🆕 validate the input range

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,3 +1,6 @@
 export function fizzBuzz(number: number) {
+  if (number < 1 || number > 100) {
+    throw new RangeError("The number should be in the interval [1, 100].");
+  }
   return "";
 }
--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -3,4 +3,13 @@ describe("fizzbuzz", () => {
   it("returns a string", () => {
     expect(typeof fizzBuzz(5)).toBe("string");
   });
+
+  it("takes only numbers from 1 to 100", () => {
+    expect(typeof fizzBuzz(1)).toBe("string");
+    expect(() => fizzBuzz(-1)).toThrowError();
+    expect(() => fizzBuzz(-25)).toThrowError();
+    expect(typeof fizzBuzz(25)).toBe("string");
+    expect(typeof fizzBuzz(100)).toBe("string");
+    expect(() => fizzBuzz(101)).toThrowError();
+  });
 });
