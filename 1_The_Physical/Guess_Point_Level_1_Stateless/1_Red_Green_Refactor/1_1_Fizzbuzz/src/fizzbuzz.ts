29947aa98ef673b5aa80321a4b11c5f91c4c4aef
todoroff <48935314+todoroff@users.noreply.github.com>
2023-03-26 01:12:37 +0200

    [New]: return 'Fizz' for multiples of 3 but not 5

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -2,5 +2,8 @@ export function fizzbuzz(num: number) {
   if (num % 15 === 0) {
     return "FizzBuzz";
   }
+  if (num % 3 === 0) {
+    return "Fizz";
+  }
   return num.toString();
 }
--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -11,4 +11,10 @@ describe("fizzbuzz", () => {
       expect(fizzbuzz(number)).toEqual("FizzBuzz");
     });
   });
+
+  it("returns 'Fizz' for multiples of 3 but not 5", () => {
+    [3, 6, 9, 12, 18].forEach((number) => {
+      expect(fizzbuzz(number)).toEqual("Fizz");
+    });
+  });
 });
