16a3f74c4d66271a8093b9cd1e7b7cff5aa344a7
todoroff <48935314+todoroff@users.noreply.github.com>
2023-03-26 01:03:14 +0200

    [New]: returns numbers as strings

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,5 +1,8 @@
-
+import { fizzbuzz } from "./fizzbuzz";
 describe("fizzbuzz", () => {
-
-
+  it("returns numbers as strings", () => {
+    [1, 2, 4, 5, 7, 8].forEach((number) => {
+      expect(fizzbuzz(number)).toEqual(number.toString());
+    });
+  });
 });
