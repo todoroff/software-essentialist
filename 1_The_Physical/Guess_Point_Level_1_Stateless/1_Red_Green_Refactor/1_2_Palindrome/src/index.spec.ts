ab2ef956028120d51f930e336d4886ddea2a198b
todoroff <48935314+todoroff@users.noreply.github.com>
2023-03-26 22:44:53 +0300

    [Refactor]: add extra test example

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -16,4 +16,8 @@ describe("palindrome checker", () => {
   it('can tell that the phrase "Was It A Rat I Saw" is a palindrome', () => {
     expect(isPalindrome("Was It A Rat I Saw")).toBeTruthy();
   });
+
+  it('can tell that the phrase "Never Odd or Even" is a palindrome', () => {
+    expect(isPalindrome("Never Odd or Even")).toBeTruthy();
+  });
 });
