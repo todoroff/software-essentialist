8c6f127fdd5d71d258930879a266c3713778f7c2
todoroff <48935314+todoroff@users.noreply.github.com>
2023-03-26 22:47:23 +0300

    [Refactor]: extract helpers for reversing a string and stripping spaces

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.ts
@@ -1,6 +1,14 @@
 export function isPalindrome(str: string) {
   return (
     str.toLowerCase().split(" ").join("") ===
-    str.split("").reverse().join("").toLowerCase().split(" ").join("")
+    stripSpaces(reverseString(str)).toLowerCase()
   );
+}
+
+function reverseString(str: string) {
+  return str.split("").reverse().join("");
+}
+
+function stripSpaces(str: string) {
+  return str.split(" ").join("");
 }
