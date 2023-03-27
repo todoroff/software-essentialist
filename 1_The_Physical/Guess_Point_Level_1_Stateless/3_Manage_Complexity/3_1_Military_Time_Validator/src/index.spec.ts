0a9a98726340c52be7759342bec2a430d6f2528c
todoroff <48935314+todoroff@users.noreply.github.com>
2023-03-28 00:42:25 +0300

    [New]: return false for "25:00 - 12:23" - start hour out of max range

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -3,4 +3,8 @@ describe("military time validator", () => {
   it('knows that "01:12 - 14:32" is valid', () => {
     expect(validateTimeRange("01:12 - 14:32")).toBeTruthy();
   });
+
+  it('knows that "25:00 - 12:23" is invalid (start hour out of max range)', () => {
+    expect(validateTimeRange("25:00 - 12:23")).toBeFalsy();
+  });
 });
--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -1,3 +1,4 @@
 export function validateTimeRange(timeRange: string): boolean {
-  return true;
+  const MAX_HOUR = 23;
+  return Number(timeRange.slice(0, 2)) <= MAX_HOUR;
 }
