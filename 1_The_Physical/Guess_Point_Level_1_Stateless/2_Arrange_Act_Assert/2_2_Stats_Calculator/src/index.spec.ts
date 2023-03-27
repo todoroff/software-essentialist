cfa47d232e579991c49ce85227fe81a056a2379f
todoroff <48935314+todoroff@users.noreply.github.com>
2023-03-27 20:21:08 +0300

    [New]: return min value, max value, avg value, and number of elements

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
@@ -1,5 +1,11 @@
+import { calculateStats } from "./index";
 
-describe('stats calculator', () => {
-
-
-})
+describe("stats calculator", () => {
+  it("returns min value, max value, avg value, and number of elements", () => {
+    const result = calculateStats([2, 4, 21, -8, 53, 40]);
+    expect(result.minValue).toBeDefined();
+    expect(result.maxValue).toBeDefined();
+    expect(result.avgValue).toBeDefined();
+    expect(result.numElements).toBeDefined();
+  });
+});
---
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.ts
@@ -0,0 +1,8 @@
+export function calculateStats(sequence: number[]) {
+  return {
+    minValue: 0,
+    maxValue: 0,
+    avgValue: 0,
+    numElements: 0,
+  };
+}
