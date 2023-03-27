57efdb28bef90f90eafe2827355a6e0fd72bb630
todoroff <48935314+todoroff@users.noreply.github.com>
2023-03-27 20:47:36 +0300

    [Refactor]: parametrize tests for stat calculations

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
@@ -9,23 +9,25 @@ describe("stats calculator", () => {
     expect(result.numElements).toBeDefined();
   });
 
-  it("returns min value of -8 for [2, 4, 21, -8, 53, 40]", () => {
-    const result = calculateStats([2, 4, 21, -8, 53, 40]);
-    expect(result.minValue).toEqual(-8);
-  });
-
-  it("returns max value of 53 for [2, 4, 21, -8, 53, 40]", () => {
-    const result = calculateStats([2, 4, 21, -8, 53, 40]);
-    expect(result.maxValue).toEqual(53);
-  });
-
-  it("returns avg value of 18.666666666667 for [2, 4, 21, -8, 53, 40]", () => {
-    const result = calculateStats([2, 4, 21, -8, 53, 40]);
-    expect(result.avgValue).toEqual(18.666666666667);
-  });
-
-  it("returns num elements of 6 for [2, 4, 21, -8, 53, 40]", () => {
-    const result = calculateStats([2, 4, 21, -8, 53, 40]);
-    expect(result.numElements).toEqual(6);
-  });
+  it.each`
+    sequence                  | stat             | value
+    ${[2, 4, 21, -8, 53, 40]} | ${"minValue"}    | ${-8}
+    ${[2, 4, 21, -8, 53, 40]} | ${"maxValue"}    | ${53}
+    ${[2, 4, 21, -8, 53, 40]} | ${"avgValue"}    | ${18.666666666667}
+    ${[2, 4, 21, -8, 53, 40]} | ${"numElements"} | ${6}
+  `(
+    'verifies "$stat" is $value for $sequence',
+    ({
+      sequence,
+      stat,
+      value,
+    }: {
+      sequence: number[];
+      stat: keyof ReturnType<typeof calculateStats>;
+      value: number;
+    }) => {
+      const result = calculateStats(sequence);
+      expect(result[stat]).toEqual(value);
+    }
+  );
 });
