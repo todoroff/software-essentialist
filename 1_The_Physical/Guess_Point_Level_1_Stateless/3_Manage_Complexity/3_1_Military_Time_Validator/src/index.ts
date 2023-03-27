f493fe5481ad5c8d9204b746d1265cc401c6336f
todoroff <48935314+todoroff@users.noreply.github.com>
2023-03-28 01:32:49 +0300

    [Refactor]: extract function that splits time ranges into start/end hours/minutes parts

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -2,14 +2,27 @@ export function validateTimeRange(timeRange: string): boolean {
   const MAX_HOUR = 23;
   const MAX_MINUTE = 59;
 
-  const startHour = timeRange.split(":")[0];
-  const startMinute = timeRange.split(":")[1].split("-")[0].trim();
-  const endHour = timeRange.split("-")[1].split(":")[0];
-  const endMinute = timeRange.split("-")[1].split(":")[1].split("-")[0].trim();
+  const { startHour, startMinute, endHour, endMinute } =
+    getTimeRangeParts(timeRange);
+
   return (
-    Number(startHour) <= MAX_HOUR &&
-    Number(endHour) <= MAX_HOUR &&
-    Number(startMinute) <= MAX_MINUTE &&
-    Number(endMinute) <= MAX_MINUTE
+    startHour <= MAX_HOUR &&
+    endHour <= MAX_HOUR &&
+    startMinute <= MAX_MINUTE &&
+    endMinute <= MAX_MINUTE
   );
+}
+
+function getTimeRangeParts(timeRange: string) {
+  const [startTime, endTime] = timeRange.split("-").map((time) => time.trim());
+  const [startHour, startMinute, endHour, endMinute] = [startTime, endTime]
+    .map((time) => time.split(":"))
+    .flat()
+    .map((timePart) => Number(timePart));
+  return {
+    startHour,
+    startMinute,
+    endHour,
+    endMinute,
+  };
 }
