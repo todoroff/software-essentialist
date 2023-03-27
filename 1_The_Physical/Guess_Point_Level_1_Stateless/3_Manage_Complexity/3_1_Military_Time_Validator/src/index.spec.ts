9234911149695cc94268cad813dfb51349a3b767
todoroff <48935314+todoroff@users.noreply.github.com>
2023-03-28 02:15:19 +0300

    [Refactor]: parametrize tests and add extra examples

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -1,38 +1,24 @@
 import { validateTimeRange } from "./index";
 describe("military time validator", () => {
-  it('knows that "01:12 - 14:32" is valid', () => {
-    expect(validateTimeRange("01:12 - 14:32")).toBeTruthy();
+  it.each([
+    ["01:12 - 14:32"],
+    ["23:59 - 00:00"],
+    ["22:00 - 23:12"],
+    ["15:33 - 04:21"],
+  ])("knows that %p is valid", (timeRange: string) => {
+    expect(validateTimeRange(timeRange)).toBeTruthy();
   });
 
-  it('knows that "23:59 - 00:00" is valid', () => {
-    expect(validateTimeRange("23:59 - 00:00")).toBeTruthy();
-  });
-
-  it('knows that "25:00 - 12:23" is invalid (start hour out of max range)', () => {
-    expect(validateTimeRange("25:00 - 12:23")).toBeFalsy();
-  });
-
-  it('knows that "11:60 - 12:23" is invalid (start minute out of max range)', () => {
-    expect(validateTimeRange("11:60 - 12:23")).toBeFalsy();
-  });
-
-  it('knows that "12:23 - 25:00" is invalid (end hour out of max range)', () => {
-    expect(validateTimeRange("12:23 - 25:00")).toBeFalsy();
-  });
-
-  it('knows that "-00:11 - 00:15" is invalid (bad format)', () => {
-    expect(validateTimeRange("-00:11 - 00:15")).toBeFalsy();
-  });
-
-  it('knows that "-1:11 - 00:15" is invalid (bad format)', () => {
-    expect(validateTimeRange("-1:11 - 00:15")).toBeFalsy();
-  });
-
-  it('knows that "125:00 - 12:23" is invalid (bad format)', () => {
-    expect(validateTimeRange("125:00 - 12:23")).toBeFalsy();
-  });
-
-  it('knows that "aa:bb - cc:dd" is invalid (bad format)', () => {
-    expect(validateTimeRange("aa:bb - cc:dd")).toBeFalsy();
+  it.each`
+    timeRange           | reasonInvalid
+    ${"25:00 - 12:23"}  | ${"start hour out of max range"}
+    ${"11:60 - 12:23"}  | ${"start minute out of max range"}
+    ${"12:23 - 25:00"}  | ${"end hour out of max range"}
+    ${"-00:11 - 00:15"} | ${"bad format"}
+    ${"-1:11 - 00:15"}  | ${"bad format"}
+    ${"125:00 - 12:23"} | ${"bad format"}
+    ${"aa:bb - cc:dd"}  | ${"bad format"}
+  `('knows that "$timeRange" is invalid ($reasonInvalid)', ({ timeRange }) => {
+    expect(validateTimeRange(timeRange)).toBeFalsy();
   });
 });
