da7f34e9a85fac2085a21ee3f7d53813680ebbe5
todoroff <48935314+todoroff@users.noreply.github.com>
2023-03-30 02:41:42 +0300

    [Refactor]: set the regex and replacer as props on each operator

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.ts
@@ -14,32 +14,41 @@ const primitives = {
 };
 
 const operators = {
-  NOT: (primitive: Primitive): Primitive =>
-    boolToPrimitive(!primitiveToBool(primitive)),
-  AND: (a: Primitive, b: Primitive): Primitive =>
-    boolToPrimitive(primitiveToBool(a) && primitiveToBool(b)),
-  OR: (a: Primitive, b: Primitive): Primitive =>
-    boolToPrimitive(primitiveToBool(a) || primitiveToBool(b)),
+  PARENS: {
+    fn: expressionToPrimitive,
+    regex: /\((\w+)\)/,
+    replacer: (match: string, expression: string) =>
+      operators.PARENS.fn(expression),
+  },
+  NOT: {
+    fn: (primitive: Primitive): Primitive =>
+      boolToPrimitive(!primitiveToBool(primitive)),
+    regex: /NOT (TRUE|FALSE)/,
+    replacer: (match: string, primitive: Primitive) =>
+      operators.NOT.fn(primitive),
+  },
+  AND: {
+    fn: (a: Primitive, b: Primitive): Primitive =>
+      boolToPrimitive(primitiveToBool(a) && primitiveToBool(b)),
+    regex: /(TRUE|FALSE) AND (TRUE|FALSE)/,
+    replacer: (match: string, primitiveA: Primitive, primitiveB: Primitive) =>
+      operators.AND.fn(primitiveA, primitiveB),
+  },
+  OR: {
+    fn: (a: Primitive, b: Primitive): Primitive =>
+      boolToPrimitive(primitiveToBool(a) || primitiveToBool(b)),
+    regex: /(TRUE|FALSE) OR (TRUE|FALSE)/,
+    replacer: (match: string, primitiveA: Primitive, primitiveB: Primitive) =>
+      operators.OR.fn(primitiveA, primitiveB),
+  },
 };
 
 function expressionToPrimitive(expression: string): Primitive {
   return expression
-    .replace(/\((\w+)\)/, (match, expression) => {
-      return expressionToPrimitive(expression);
-    })
-    .replace(/NOT (TRUE|FALSE)/, (match, primitive: Primitive) =>
-      operators.NOT(primitive)
-    )
-    .replace(
-      /(TRUE|FALSE) AND (TRUE|FALSE)/,
-      (match, primitiveA: Primitive, primitiveB: Primitive) =>
-        operators.AND(primitiveA, primitiveB)
-    )
-    .replace(
-      /(TRUE|FALSE) OR (TRUE|FALSE)/,
-      (match, primitiveA: Primitive, primitiveB: Primitive) =>
-        operators.OR(primitiveA, primitiveB)
-    ) as Primitive;
+    .replace(operators.PARENS.regex, operators.PARENS.replacer)
+    .replace(operators.NOT.regex, operators.NOT.replacer)
+    .replace(operators.AND.regex, operators.AND.replacer)
+    .replace(operators.OR.regex, operators.OR.replacer) as Primitive;
 }
 
 function primitiveToBool(primitive: Primitive): boolean {
