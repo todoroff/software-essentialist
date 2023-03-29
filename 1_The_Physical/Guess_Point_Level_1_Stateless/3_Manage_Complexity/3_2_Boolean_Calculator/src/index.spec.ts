a57d72529cd72c618d55f9692142bd0e0c4ca640
todoroff <48935314+todoroff@users.noreply.github.com>
2023-03-30 01:05:57 +0300

    [Refactor]: parametrize tests

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
@@ -1,14 +1,10 @@
 import { boolCalculator } from "./index";
 describe("boolean calculator", () => {
-  it('evaluates the expression "TRUE" to true', () => {
-    expect(boolCalculator("TRUE")).toBeTruthy();
-  });
-
-  it('evaluates the expression "FALSE" to false', () => {
-    expect(boolCalculator("FALSE")).toBeFalsy();
-  });
-
-  it('evaluates the expression "NOT FALSE" to true', () => {
-    expect(boolCalculator("NOT FALSE")).toBeTruthy();
+  it.each([
+    ["TRUE", true],
+    ["FALSE", false],
+    ["NOT FALSE", true],
+  ])("evaluates %p to %p", (expression: string, result: boolean) => {
+    expect(boolCalculator(expression)).toEqual(result);
   });
 });
