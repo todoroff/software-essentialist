cfa7be9f71529a6bd8b5114164e2f242c25f3e06
todoroff <48935314+todoroff@users.noreply.github.com>
2023-03-30 01:24:29 +0300

    [Refactor]: test all possibilities for "AND" operator

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
@@ -6,6 +6,9 @@ describe("boolean calculator", () => {
     ["NOT FALSE", true],
     ["NOT TRUE", false],
     ["TRUE AND FALSE", false],
+    ["FALSE AND TRUE", false],
+    ["FALSE AND FALSE", false],
+    ["TRUE AND TRUE", true],
   ])("evaluates %p to %p", (expression: string, result: boolean) => {
     expect(boolCalculator(expression)).toEqual(result);
   });
