8883e15d6dbfa36f632eb6d94553d67fe097d968
todoroff <48935314+todoroff@users.noreply.github.com>
2023-03-29 21:12:09 +0300

    [New]: evaluate the expression "TRUE" to true

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
@@ -1,4 +1,6 @@
-
-describe('boolean calculator', () => {
-
-})
+import { boolCalculator } from "./index";
+describe("boolean calculator", () => {
+  it('evaluates the expression "TRUE" to true', () => {
+    expect(boolCalculator("TRUE")).toBeTruthy();
+  });
+});
