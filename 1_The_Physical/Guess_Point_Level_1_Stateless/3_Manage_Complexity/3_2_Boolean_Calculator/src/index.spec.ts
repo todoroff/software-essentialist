ef404eaaa9ec88a1ded8b724349e64ca07338946
todoroff <48935314+todoroff@users.noreply.github.com>
2023-03-30 01:34:39 +0300

    [Refactor]: categorize tests

--- 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
@@ -1,16 +1,26 @@
 import { boolCalculator } from "./index";
+
 describe("boolean calculator", () => {
-  it.each([
-    ["TRUE", true],
-    ["FALSE", false],
-    ["NOT FALSE", true],
-    ["NOT TRUE", false],
-    ["TRUE AND FALSE", false],
-    ["FALSE AND TRUE", false],
-    ["FALSE AND FALSE", false],
-    ["TRUE AND TRUE", true],
-    ["TRUE OR FALSE", true],
-  ])("evaluates %p to %p", (expression: string, result: boolean) => {
-    expect(boolCalculator(expression)).toEqual(result);
+  describe("primitives", () => {
+    it.each([
+      ["TRUE", true],
+      ["FALSE", false],
+    ])("evaluates %p to %p", (expression: string, result: boolean) => {
+      expect(boolCalculator(expression)).toEqual(result);
+    });
+  });
+
+  describe("operators", () => {
+    it.each([
+      ["NOT FALSE", true],
+      ["NOT TRUE", false],
+      ["TRUE AND FALSE", false],
+      ["FALSE AND TRUE", false],
+      ["FALSE AND FALSE", false],
+      ["TRUE AND TRUE", true],
+      ["TRUE OR FALSE", true],
+    ])("evaluates %p to %p", (expression: string, result: boolean) => {
+      expect(boolCalculator(expression)).toEqual(result);
+    });
   });
 });
